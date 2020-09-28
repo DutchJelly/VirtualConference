0ccd01e349917ecffe31f7c6315d2e657636f34f
Jelle Keulemans <jelle.keulemans@gmail.com>
2020-09-28 22:46:34 +0200

    added frontend files

---
+++ loginvoorbeeld/frontend/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="main-container">
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+.main-container {
+  @apply bg-gray-200 min-h-screen w-full flex flex-col items-center;
+}
+</style>
---
+++ loginvoorbeeld/frontend/pages/overview.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Overview</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
