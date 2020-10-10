fae7b90bd438b0a791af48365b19d597c063304f
DutchJelly <jelle.keulemans@gmail.com>
2020-10-10 02:28:22 +0200

    Frontend alle functies voor request handling

--- sprint2/frontend/pages/jitsi.vue
+++ sprint2/frontend/pages/jitsi.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="main">
-      <h1>{{message}}</h1>
-      <ul><li v-for="(item, index) in users" :key="index"><button class="user" @click.prevent="sendConversationRequest(item)">{{item}}</button></li></ul>
-      <ConformationPrompt v-if="pendingRequest" user="test" />
+    
+    <ConformationPrompt 
+        class="absolute-center" 
+        v-if="conversationRequest.pending"
+        :onAccept="conversations().acceptRequest"
+        :onDecline="conversations().declineRequest"
+        :user="conversationRequest.user" 
+    />
+
+    <TimedInfoMessageBox v-if="info" :message="info" time="5"/>
+    <ul>
+        <li v-for="(item, index) in users" :key="index">
+            <button class="user" @click.prevent="conversations().sendRequest(item)"> 
+                {{item}} 
+            </button>
+        </li>
+    </ul>
+      
   </div>
 </template>
 
 <script>
-// import Vue from 'vue'
-// import VueSocketIO from 'vue-socket.io'
-
-// import Vue from 'vue';
 import moment from 'moment';
 
 
@@ -19,10 +30,14 @@ export default {
     data(){
         return{
             username: this.$route.query.username,
-            message: "",
+            info: "",
             users: [],
-            pendingRequest: false,
             socket: null,
+            conversationRequest: {
+                pending: false,
+                user: "none",
+                pendingUsers: []
+            }
         };
     },
     async mounted(){
@@ -33,12 +48,28 @@ export default {
         this.socket = this.$nuxtSocket({
             name: "home",
         });
+
         //Incoming socket events:
         this.socket.on("requestConversation", (data) => {
+            if(!data || !data.user) return;
 
+            if(this.conversationRequest.pending){
+                if(!this.conversationRequest.pendingUsers.includes(this.conversationRequest.user)){
+                    this.conversationRequest.pendingUsers.push(data.user);
+                    this.info = `You received an conversation request from ${data.user}.`;
+                }
+                return;
+            }
+            this.conversationRequest.user = data.user;
+            this.conversationRequest.pending = true;
+            this.info = `You received an conversation request from ${data.user}.`;
         });
-        this.socket.on("message", (data) => {
-            console.log("Received this message from the server:", data);
+
+        //Temporary way to handle users 'loggin on'
+        this.socket.on("testlogin", (data) => {
+            if(!data || !data.online) return;
+
+            this.users = data.online;
         });
 
         //The server doesn't know what user is using this socket, tell the server.
@@ -46,21 +77,56 @@ export default {
 
         this.users = response.data.online;
     },
-    
     methods: {
-        sendConversationRequest: async function(withWho){
-            console.log("hello world");
-            if(this.username === withWho){
-                this.message = "you cannot click on yourself";
-                return;
+
+        //Nested functions are not possible. This is the next best thing.
+        conversations: function(){
+            var self = this;
+            
+            //Return all functions as json.
+            return{
+                sendRequest: async function(withWho){
+                    console.log("hello world");
+                    if(self.username === withWho){
+                        self.message = "you cannot click on yourself";
+                        return;
+                    }
+                    self.message = "";
+                    await self.$axios(`http://localhost:5000/requestconversation/${self.username}/${withWho}`);
+                },
+                acceptRequest: function(withWho){
+                    self.socket.emit("acceptJitsiRequest", {user: self.username, withWho});
+                    self.conversationRequest.pending = false;
+                    self.conversationRequest.user = "none";
+
+                    //Automatically decline all other requests that were sent after the accepted one.
+                    self.conversationRequest.pendingQueue.forEach(element => {
+                        this.declineRequest(element);
+                    });
+                    self.conversationRequest.pendingQueue = [];
+                },
+                declineRequest: function(withWho){
+                    self.socket.emit("declineJitsiRequest", {user: self.username, withWho});
+                    if(self.conversationRequest.pendingQueue.length){
+                        self.conversationRequest.user = pendingQueue[0];
+                        pendingQueue.shift();
+                        return;
+                    }
+                    self.conversationRequest.pending = false;
+                    self.conversationRequest.user = "none";
+                }
             }
-            this.message = "";
-            await this.$axios(`http://localhost:5000/requestconversation/${this.username}/${withWho}`);
         }
     }
-   
 };
 </script>
 
 <style>
+.absolute-center{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%)
+}
+
 </style>
