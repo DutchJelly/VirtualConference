d75a73ecd13d5336bd1e6bef9f34a851d396359a
DutchJelly <jelle.keulemans@gmail.com>
2020-10-12 20:14:41 +0200

    removed sprint folder because we're using branches to manage sprint versions

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="main-container">
+    <Nuxt />
+</div>
+
+  
+</template>
+
+<style>
+.main-container {
+  @apply bg-gray-200 min-h-screen w-full flex flex-col items-center;
+  overflow-x: hidden;
+  overflow-y: hidden;
+}
+</style>
+
+
+<script>
+
+
+export default {
+  mounted(){
+    
+  }
+  
+}
+</script>
---
+++ frontend/pages/kamerview.vue
@@ -0,0 +1,46 @@
+<template>
+  <main class="kamerpage">
+    <div class="kamer">
+      <UserIcon v-bind:items="items"></UserIcon>
+    </div>
+    <Sidebar roomName="templateRoom01" v-bind:items="items"></Sidebar>
+  </main>
+</template>
+
+<script>
+
+export default {
+
+  data() {
+    
+    return { 
+      items: [
+        { user: 'Richard' },
+        { user: 'Wouter' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+.kamerpage{
+  @apply w-full min-h-screen flex flex-col items-center justify-center;
+  background: black url("../static/login_background.svg") no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  transform: scale(1.02);
+}
+
+.kamer{
+  @apply bg-white rounded shadow-lg p-5;
+  height: 90%;
+  width: 82%;
+  left: 3%;
+  position: fixed;
+}
+
+</style>
