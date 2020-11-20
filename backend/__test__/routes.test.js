09e185b72177e16f81e4dab8db548095608edc34
Luuk van den Nouweland <l.van.den.nouweland@umail.leidenuniv.nl>
2020-11-20 14:08:36 +0100

    testen?

---
+++ backend/__test__/routes.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest')
+const app = require('../src/routes')
+
+// import app from "../src/routes"
+describe('create_user', () => {
+  it('should create a new user', async () => {
+    const res = await request(app)
+      .post('/create_user')
+      .send({
+        username: 'test@test.com',
+        password: 'test1234',
+      })
+    expect(res.statusCode).toEqual(201)
+    expect(res.body).toHaveProperty('json')
+  })
+})
