4eedaebadc33bb89c48bca2cd2f5dd0d67f4bfab
Richard <richard-drahcir@hotmail.com>
2020-11-20 17:22:59 +0100

    added testSuite

---
+++ frontend/pages/editPlattegrond.vue
@@ -0,0 +1,22 @@
+<template>
+    <mainDiv class="main">
+        <iframe src="YANC21-floorplan.sozi.html">
+        </iframe>
+    </mainDiv>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style>
+.main {
+    width:100%;
+    height:100%;
+}
+iframe {
+    height:100%;
+    width: 100%;
+}
+</style>
