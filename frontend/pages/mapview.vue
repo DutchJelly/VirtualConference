e984fa3eb629014f5fa395ede9017dd6acb41aee
Richard <richard-drahcir@hotmail.com>
2020-12-12 13:45:34 +0100

    Added some flair

--- frontend/pages/mapview.vue
+++ frontend/pages/mapview.vue
@@ -3,7 +3,15 @@
         <button class="logout" @click.prevent="logout">log out</button>
         <iframe id="ifr" src="YANC21-floorplan.sozi.html">
         </iframe>
-
+        <div v-if="visible" class="information">
+            <button class="information-button" @click="visible=false">X</button>
+            Welcome to Virtual Conference! <br>
+            Please click on a room to start or<br>
+            click in the top right to change floors<br>
+            You are currently hovering over Room: <br>
+            {{roomNumber}}
+        </div>
+        <button v-if="!visible" class="help" @click="visible=true">i</button>
     </div>
 </template>
 
@@ -14,6 +22,13 @@
 import { mapState } from 'vuex'
 export default {
     middleware: 'auth',
+
+    data() {
+        return {
+            roomNumber: "",
+            visible: true,
+        }
+    },
     async mounted() {
         this.$nextTick(() => {
             let iframe = document.getElementById('ifr')
@@ -23,6 +38,9 @@ export default {
                     if(element.getAttribute("inkscape:label")) {
                         element.addEventListener("click", () => {
                             this.$router.push({ path:"/roomview", query: element.id })
+                        })
+                        element.addEventListener("mouseover", () => {
+                            this.roomNumber = element.getAttribute("inkscape:label")
                         })
                     }
                     
@@ -71,4 +89,25 @@ iframe {
 .logout:focus {
   @apply bg-green-600;
 }
+
+.information{
+   @apply shadow bg-green-700 mt-3 text-xl text-white p-2 rounded border-2 border-transparent;
+    position: absolute;
+    top: 2%;
+    right: 2%;
+}
+
+.information-button{
+    right: 1%;
+    top: 1%;
+    position: absolute;
+}
+
+.help{
+    @apply shadow bg-green-700 mt-3 text-4xl text-white p-2 rounded-full h-20 w-20 border-2 border-transparent;
+    position: absolute;
+    top: 2%;
+    right: 2%;
+}
+
 </style>
